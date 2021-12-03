a1a8bf6b3274679791498b2305cd01132384a27e
Vinh Pham <phamv1@unlv.nevada.edu>
2021-12-02 23:56:02 -0800

    completed bonus, remove and add book

--- public/admin.js
+++ public/admin.js
@@ -10,10 +10,11 @@ async function getBookList() {
 function displayBookList(book) {
     let bookAdminContainer = document.querySelector('#admin-list')
     bookAdminContainer.innerHTML += `
-        <li>
-            <label for="${'bookId'+book.id}">${book.title}</label>
-            <input id="${'bookId'+book.id}" value="${book.quantity}"/>
-            <button onClick="saveToInventory(${book.id})">Save</button>
+        <li class="row py-1" id="${'bookContainer'+book.id}">
+            <label class="col-2" for="${'bookId'+book.id}">${book.title}</label>
+            <input class="col-2" id="${'bookId'+book.id}" value="${book.quantity}"/>
+            <button class="col-1 mx-1 btn btn-primary" type="button" onClick="saveToInventory(${book.id})">Save</button>
+            <button  type="button" class="btn btn-danger col-2" onClick="removeBook(${book.id})">Remove</button>
         </li>
     `
 }
@@ -38,5 +39,60 @@ async function saveToInventory(bookId) {
     })
 
     const result = await response.json();
+    //console.log(result)
+    if(result.error) {
+        alert(result.message)
+    } else {
+        alert("Successfully save!")
+    }
+}
+
+async function removeBook(bookId) {
+    //
+    console.log(bookId)
+    const response = await fetch(`http://localhost:3001/removeBook/${bookId}`,{
+        method: 'DELETE',
+    })
+
+    const result = await response.json();
     console.log(result)
+    if(result.error) {
+        alert(result.message);
+    } else {
+        let idSelector = '#bookContainer'+bookId;
+        let itemToRemove = document.querySelector(idSelector);
+        itemToRemove.remove();
+        alert('Book removed successfully!');
+    }
+}
+
+async function addNewBook() {
+    event.preventDefault()
+    console.log("adding book")
+
+    let bookTitle = document.querySelector("#new-book-title").value;
+    let bookDescription = document.querySelector("#book-discription").value;
+    let bookImgUrl = document.querySelector("#book-img-url").value;
+    let quantity =  document.querySelector("#book-quantity").value;
+
+    const response = await fetch("http://localhost:3001/addBook", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "title": bookTitle,
+            "description": bookDescription,
+            "imageURL": bookImgUrl,
+            "quantity": quantity
+        })
+    })
+     const result = await response.json()
+
+     if(result.error) {
+         alert(result.message)
+     } else {
+         console.log(result)
+         displayBookList(result)
+     }
 }
